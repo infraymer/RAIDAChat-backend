be7b160906652cc151c3e67526659dda487ea416
Unknown <sergey020894@mail.ru>
2017-11-15 18:44:40 +0500

    С БД
    Добавил дамп базы данных

--- RAIDAChat/Models/CloudChat.Context.cs
+++ RAIDAChat/Models/CloudChat.Context.cs
@@ -20,7 +20,7 @@ namespace RAIDAChat.Models
         public CloudChatEntities()
             : base("name=CloudChatEntities")
         {
-            //Database.SetInitializer(new DropCreateDatabaseIfModelChanges<CloudChatEntities>());
+         //   Database.SetInitializer(new DropCreateDatabaseIfModelChanges<CloudChatEntities>());
         }
     
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
